21367d157894e00ae27228a61d7e966ea3a81780
AbrRahman <abrahman.est@gamil.com>
2023-05-02 01:17:31 +0600

    pattern 2 print

--- pattern_2.c
+++ pattern_2.c
@@ -3,23 +3,17 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int s=n-1,p=1;
+    int p=2*n-1,s=1;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=s;j++){
+        for(int j=1;j<s;j++){
             printf(" ");
         }
         for(int j=1;j<=p;j++){
             printf("*");
         }
-        s--;
-        p++;
+        s++;
+        p-=2;
         printf("\n");
-// output
-//     *
-//    **
-//   ***
-//  ****
-// *****
     }
     return 0;
 }
